dfeb3e6880db8a82f4fbb726bdccb25ad6914df5
JoaoDaviMNunes <jd.mn@hotmail.com>
2025-01-15 00:32:22 -0300

    Pequeno avanço, a terminar

--- keys_generator.py
+++ keys_generator.py
@@ -1,18 +1,20 @@
 import string
 
-def salvar_chave_fraca(chave):
-    with open('chaves_fracas.txt','a') as saida:
-        saida.write(chave)
-        saida.write('\n')
+def salvar_chave_fraca(chaves):
+    with open('chaves_fracas.txt','w') as saida:
+        for key in chaves:
+            saida.write(key)
+            saida.write('\n')
 
-def salvar_chave_forte(chave):
-    with open('chaves_fortes.txt','a') as saida:
-        saida.write(chave)
-        saida.write('\n')
+def salvar_chave_forte(chaves):
+    with open('chaves_fortes.txt','w') as saida:
+        for key in chaves:
+            saida.write(key)
+            saida.write('\n')
 
 # Função para gerar a próxima combinação dos 6 caracteres finais
 def incrementar_chave_forte(chars, chave):
-    parte_variavel = chave[8:]  # Parte variável da chave (últimos 6 caracteres)
+    parte_variavel = chave[10:]  # Parte variável da chave (últimos 6 caracteres)
     lista_chave = list(parte_variavel)
     i = len(lista_chave) - 1
     while i >= 0:
@@ -25,7 +27,7 @@ def incrementar_chave_forte(chars, chave):
     else:
         return None  # Não há mais combinações possíveis
 
-    return chave[:8] + ''.join(lista_chave)  # Combina a parte fixa com a parte variável
+    return 'Security00' + ''.join(lista_chave)  # Combina a parte fixa com a parte variável
 
 # Função para gerar a próxima combinação dos 5 caracteres finais
 def incrementar_chave_fraca(chars, chave):
@@ -51,15 +53,19 @@ chars = string.ascii_letters + string.digits
 chave_inicial_fraca = "SecurityAES00000"
 chave_inicial_forte = 'Security00000000'
 
+# Listas de chaves:
+fracas, fortes = [],[]
+
 while chave_inicial_fraca != "SecurityAESFFFFF":
-    salvar_chave_fraca(chave_inicial_fraca)
+    fracas.append(chave_inicial_fraca)
     chave_inicial_fraca = incrementar_chave_fraca(chars, chave_inicial_fraca)
+fracas.append(chave_inicial_fraca)    
 
 while chave_inicial_forte != "Security00FFFFFF":
-    salvar_chave_forte(chave_inicial_forte)
+    fortes.append(chave_inicial_forte)
+    print(chave_inicial_forte)
     chave_inicial_forte = incrementar_chave_forte(chars, chave_inicial_forte)
+fortes.append(chave_inicial_forte)
 
 salvar_chave_fraca(chave_inicial_fraca)
-chave_inicial_fraca = incrementar_chave_fraca(chars, chave_inicial_fraca)
 salvar_chave_forte(chave_inicial_forte)
-chave_inicial_forte = incrementar_chave_forte(chars, chave_inicial_forte)
